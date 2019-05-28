85c60146447dc57e09e10139dea5beb801ce7d48
Nakul Kumar <nakul.kumar@lenskart.in>
2019-05-28 20:08:01 +0530

    Added new api

--- src/main/java/com/hystrixexample/hystrixdemoservice/controller/ThirdPartyServiceController.java
+++ src/main/java/com/hystrixexample/hystrixdemoservice/controller/ThirdPartyServiceController.java
@@ -3,10 +3,7 @@ package com.hystrixexample.hystrixdemoservice.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @RestController
 public class ThirdPartyServiceController {
@@ -15,5 +12,10 @@ public class ThirdPartyServiceController {
     public List<Integer> getNumbers(){
         Random random = new Random();
         return new ArrayList<>(Arrays.asList(random.nextInt(), random.nextInt(), random.nextInt(), random.nextInt()));
+    }
+
+    @GetMapping("/string")
+    public String getString(){
+        return UUID.randomUUID().toString();
     }
 }
